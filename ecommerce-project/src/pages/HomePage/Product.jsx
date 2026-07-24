7268f1017dcc4d3f92699bbbb7d842882ab4f646
YashPrakash <yashyp925@gmail.com>
2026-07-24 15:32:46 +0530

    Fix backend URL

--- ecommerce-project/src/pages/HomePage/Product.jsx
+++ ecommerce-project/src/pages/HomePage/Product.jsx
@@ -62,7 +62,7 @@ export function Product({product,GetCart}) {
 
                 <button className="add-to-cart-button button-primary"
                     onClick={async () => {
-                        await axios.post("http://https://ecommerce-by-yp.onrender.com/api/cart-items", {
+                        await axios.post("https://ecommerce-by-yp.onrender.com/api/cart-items", {
                             productId: product.id,
                             quantity: quantity
                         });
